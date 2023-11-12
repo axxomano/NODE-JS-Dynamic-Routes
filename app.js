5963ad11aedc437970c5d499858af3e6eb40b5c5
Mano Ekambaram <digitaldeliver@gmail.com>
2023-11-12 08:54:18 +0530

    Design the User and Product relationship

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+
 const cors = require('cors')
 const app = express();
 
@@ -21,7 +24,13 @@ const expenseRoutes = require('./routes/expenses');
 
 app.use(bodyParser.json({ extended: false })); //EJS supports .urlencoded
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req,res,next)=>{
+    User.findByPk(1).then(user=>{
+        req.user = user;
+        next()
+    })
+    .catch(err=>console.log(err))
+})
 app.use('/admin', adminRoutes);
 app.use('/users', usersRoutes);
 app.use('/expense', expenseRoutes);
@@ -29,8 +38,18 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result =>{
+Product.belongsTo(User, {constraints:true, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
+sequelize.sync({force:true}).then(result =>{
     //console.log(result)
+    return User.findByPk(1)
+}).then(user =>{
+    if(!user){
+        return User.create({name:'Mano', email:'test@test.com'})
+    }
+    return user
+}).then(user =>{
     app.listen(3000)
 })
 .catch( err => console.log(err))
